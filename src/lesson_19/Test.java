ee66adb1fe7805a3c3df3fe0e727dd250802973a
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-02-23 10:16:12 +0100

    Test file in lesson 19. Checking git connection with SSH

---
+++ src/lesson_19/Test.java
@@ -0,0 +1,11 @@
+package lesson_19;
+
+/**
+ * @author Vasilii Serebrovskii
+ * @version 1.0 (23.02.2025)
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Проверка работы git после переключения на ssh!");
+    }
+}
