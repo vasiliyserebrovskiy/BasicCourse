8f8c7dcf89ce17d0d13919cc4253842858689a3c
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-03-07 09:57:10 +0100

    professional athlete changed

--- src/homework_28/human/ProfessionalAthlete.java
+++ src/homework_28/human/ProfessionalAthlete.java
@@ -4,7 +4,7 @@ package homework_28.human;
  * @author Vasilii Serebrovskii
  * @version 1.0 (06.03.2025)
  */
-public class ProfessionalAthlete extends AmateurAthlete {
+public class ProfessionalAthlete extends Human {
 
     {
         typeRunner = "Профессионал";
